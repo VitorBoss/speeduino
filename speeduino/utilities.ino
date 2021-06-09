41120f7cf07756e449cdcc9069cf2a800ee75058
Vitor Moreno B. Sales <vitor_boss@yahoo.com.br>
2021-06-09 20:35:46 -0300

    Update utilities.ino

--- speeduino/utilities.ino
+++ speeduino/utilities.ino
@@ -577,7 +577,7 @@ int16_t ProgrammableIOGetData(uint16_t index)
     //Special cases for temperatures
     if( (index == 6) || (index == 7) ) { result -= CALIBRATION_TEMPERATURE_OFFSET; }
   }
-  else if ( index == 239U ) { result = max((int16_t)runSecsX10, 32768); }
+  else if ( index == 239U ) { result = ((int16_t)runSecsX10 > 32768)?32768:(int16_t)runSecsX10; }
   else { result = -1; } //Index is bigger than fullStatus array
   return result;
 }
